d8b209f046e83df5e5838965a804db4d2e0bb584
webdevdea <webdevdea@gmail.com>
2014-09-03 10:30:48 -0400

    Having issues with routes and controllers
    Getting this error Symfony \ Component \ HttpKernel \ Exception \
    NotFoundHttpException

--- app/routes.php
+++ app/routes.php
@@ -1,32 +1,7 @@
 <?php
 
-//Route::get('users', 'UsersController@index');
+Route::get('users', 'UsersController@index');
 
-Route::get('/', function()
-{
- // $user = new User;
- // $user->username = 'NewUser';
- // $user->password = Hash::make('password');
- // $user->save();
- 
- //User::create([
- //'username' => 'AnotherUser'
- //'password' => Hash::make('1234')
-// ]);
- // $user = User::find(5);
- // $user->username = 'UpdatedName';
- // $user->save();
- //$user = User::find(5);
- //$user->delete();
-  
-  //return User::orderBy('username', 'asc')->get();
-  
-  $users = User::all();
-  
- 
-return View::make('users/index', ['users'=>$users]);
-
-});
 
 Route::get('users/{username}', function($username)
 {

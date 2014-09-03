81397b446979fb1658597d560847b267a8dfeebd
webdevdea <webdevdea@gmail.com>
2014-09-03 09:08:28 -0400

    Working on training
    Training for the larval project

---
+++ app/routes.php
@@ -0,0 +1,40 @@
+<?php
+
+
+Route::get('/', function()
+{
+ // $user = new User;
+ // $user->username = 'NewUser';
+ // $user->password = Hash::make('password');
+ // $user->save();
+ 
+ //User::create([
+ //'username' => 'AnotherUser'
+ //'password' => Hash::make('1234')
+// ]);
+ // $user = User::find(5);
+ // $user->username = 'UpdatedName';
+ // $user->save();
+ //$user = User::find(5);
+ //$user->delete();
+  
+  //return User::orderBy('username', 'asc')->get();
+  
+  $users = User::all();
+  
+ 
+return View::make('users/index', ['users'=>$users]);
+
+});
+
+Route::get('users/{username}', function($username)
+{
+   $user = User::whereUsername($username)->first();
+   return View::make('users.show', ['user' => $user]);
+
+  
+});
+
+
+
+
